537d23be75a854d19a3a4d8a882c20504efd56c3
trik-flip <flip.philip@hotmail.com>
2020-06-20 18:56:10 +0200

    started with decrypt

--- src/App.java
+++ src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         App app = new App();
         app.encrypt("abcd", "2");
+        app.decrypt("defg", "3");
     }
 
     private String encrypt(String text, String key) {
@@ -10,5 +11,15 @@ public class App {
             System.out.print(chr);
         }
         return null;
+    }
+    private String decrypt(String text, String key){
+      char[] textArray = text.toCharArray();
+      String retText = "";
+      for (int i = 0;i < textArray.length;i++){
+        textArray[i] -= Integer.parseInt(key);
+        retText += textArray[i];
+      }
+        System.out.println(retText);
+      return retText;
     }
 }

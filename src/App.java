13d777a4e5e024f3ce27b9468150ad97faaf5dda
ShrimpRick <stefan.bollen@hotmail.com>
2020-06-20 18:52:41 +0200

    fixed: encrypt

--- src/App.java
+++ src/App.java
@@ -1,14 +1,23 @@
 public class App {
     public static void main(String[] args) throws Exception {
         App app = new App();
-        app.encrypt("abcd", "2");
+        app.encrypt("ABCabcXYZxyz", "2");
     }
 
     private String encrypt(String text, String key) {
-        for (char chr : text.toCharArray()) {
-            chr += Integer.parseInt(key);
-            System.out.print(chr);
+        char[] cs = text.toCharArray();
+        for (int i = 0; i < cs.length; i++) {
+
+            cs[i] += Integer.parseInt(key);
+            if (cs[i] > 'z') {
+                cs[i] -= 26;
+            }
+            else if (cs[i] > 'Z'&&(cs[i]-2)<'a'){
+                     cs[i] -= 26;   
+            }
         }
+
+        System.out.println(cs);
         return null;
     }
 }
